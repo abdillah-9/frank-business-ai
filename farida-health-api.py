df52954852cfb6b55fccd50cbd2fb327bdfff99e
abdillah-9 <sabdillah855@gmail.com>
2025-06-20 11:11:10 +0300

    v1.1

--- farida-health-api.py
+++ farida-health-api.py
@@ -19,7 +19,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://woman-reproduction-health-tracker.netlify.app"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
